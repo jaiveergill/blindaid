a5c15f23249e8c193976ee3409d916e9531de687
Jaiveer Gill <94658505+jaiveergill@users.noreply.github.com>
2022-12-22 11:49:54 -0800

    Added run model every 5s (15s later, 5s for test)

--- main_loop.py
+++ main_loop.py
@@ -3,13 +3,22 @@ import jetson_inference
 import os
 
 THRESHOLD = 0.2
+timestamp = 0 # in milliseconds
 
 net = jetson_inference.detectNet("ssd-mobilenet-v2", threshold=THRESHOLD)
 camera = jetson_utils.gstCamera(299, 299, "dev/video1")
 display = jetson_utils.glDisplay()
 
 while display.isOpen():
+  if 4.9 < round(timestamp, 1) < 5.1:
+    # SAVE IMAGE AND RUN MODEL
+    pass
+  
+  
   img, width, height = camera.CaptureRGBA()
   detections = net.Detect(img, width, height)
   display.renderOnce(img, width, height)
+  
+  seconds_elapsed = 1/net.GetNetworkFPS()
+  timestamp += seconds_elapsed
   
